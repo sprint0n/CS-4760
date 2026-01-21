0c9810982962cf35df65bb7c7fc1be2aa1411eec
Dawson Skalka <dawsonskalka@mail.weber.edu>
2026-01-21 15:25:40 -0700

    Application Update 1.0
    Added Textbox:
    Procedure/instruction
    IndexNumber --readonly
    UserType
    Public boolean for backend
    checkbox --extends to file upload when checked

--- University Grant Application System/Pages/ApplicationPage.cshtml.cs
+++ University Grant Application System/Pages/ApplicationPage.cshtml.cs
@@ -7,24 +7,61 @@ namespace University_Grant_Application_System.Pages
     public class ApplicationPageModel : PageModel
     {
 
-        [BindProperty]
-        [Required(ErrorMessage = "Please enter your full name")]
-        public string ApplicantName { get; set; }
 
         [BindProperty]
-        [EmailAddress]
         [Required]
-        public string Email { get; set; }
+        [Display(Name = "Index number")]
+        public string IndexNumber { get; set; }
+
+        //This is for the dropdown menu for Primaryuser || Type of user
+
+        [BindProperty]
+        [Required]
+        [Display(Name = "User")]
+        public string TypeOfUser { get; set; }
+
+        public List<string> UserTypes { get; } = new List<string>
+        {
+            "PrimaryUser",
+            "Student",
+            "Faculty",
+            "Staff",
+            "External Researcher"
+        };
+
+        //This is the name for the Procedure
+        [BindProperty]
+        [Required]
+        [Display(Name = "Procedures")]
+        public string Procedure { get; set; }
+
+
+        [BindProperty]
+        [Required(ErrorMessage = "Please enter in the title")]
+        public string GrantTitle { get; set; }
+
 
         [BindProperty]
         [Required]
         [Display(Name = "Grant Purpose")]
         public string GrantPurpose { get; set; }
 
+        /// <summary>
+        /// This is used for the Humans or Animals checkbox
+        /// True opens file upload
+        /// </summary>
+        [BindProperty]
+        public bool HumansOrAnimals { get; set; }
+
+
+
+
+
         public void OnGet()
         {
 
         }
+
 
 
         public IActionResult OnPost()

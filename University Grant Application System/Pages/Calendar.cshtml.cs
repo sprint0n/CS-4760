f604e9aee858a56ccbaa9187b42401c03a1fb8e9
sprint0n <spencerzundel@gmail.com>
2026-01-21 01:11:01 -0700

    Merged all changes together for the first sprint meeting

--- University Grant Application System/Pages/Calendar.cshtml.cs
+++ University Grant Application System/Pages/Calendar.cshtml.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace University_Grant_Application_System.Pages
 {
-    public class UserViewModel : PageModel
+    public class CalendarModel : PageModel
     {
         public void OnGet()
         {

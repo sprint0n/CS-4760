1b496dd1ff70ce24bd80b21dce97985b0d9a54b1
LoganBaty <loganbaty@mail.weber.edu>
2026-01-23 13:02:30 -0700

    Added Colleges tab to Admin Dashboard. Contains temporary list of data before DB hooked up.

---
+++ University Grant Application System/Pages/Colleges/Delete.cshtml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using University_Grant_Application_System.Data;
+using University_Grant_Application_System.Models;
+
+namespace University_Grant_Application_System.Pages.Colleges
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly University_Grant_Application_System.Data.University_Grant_Application_SystemContext _context;
+
+        public DeleteModel(University_Grant_Application_System.Data.University_Grant_Application_SystemContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public College College { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var college = await _context.Colleges.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (college is not null)
+            {
+                College = college;
+
+                return Page();
+            }
+
+            return NotFound();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var college = await _context.Colleges.FindAsync(id);
+            if (college != null)
+            {
+                College = college;
+                _context.Colleges.Remove(College);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ University Grant Application System/Pages/Colleges/Edit.cshtml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using University_Grant_Application_System.Data;
+using University_Grant_Application_System.Models;
+
+namespace University_Grant_Application_System.Pages.Colleges
+{
+    public class EditModel : PageModel
+    {
+        private readonly University_Grant_Application_System.Data.University_Grant_Application_SystemContext _context;
+
+        public EditModel(University_Grant_Application_System.Data.University_Grant_Application_SystemContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public College College { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var college =  await _context.Colleges.FirstOrDefaultAsync(m => m.Id == id);
+            if (college == null)
+            {
+                return NotFound();
+            }
+            College = college;
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(College).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CollegeExists(College.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool CollegeExists(int id)
+        {
+            return _context.Colleges.Any(e => e.Id == id);
+        }
+    }
+}

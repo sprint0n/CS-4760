6743578b7441e73b2657977e8bd332361b7c1480
LoganBaty <loganbaty@mail.weber.edu>
2026-01-30 14:35:00 -0700

    Made various changes to Admin Dashboard and its tabs

---
+++ University Grant Application System/Pages/Schools/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using University_Grant_Application_System.Data;
+using University_Grant_Application_System.Models;
+
+namespace University_Grant_Application_System.Pages.Schools
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly University_Grant_Application_System.Data.University_Grant_Application_SystemContext _context;
+
+        public DetailsModel(University_Grant_Application_System.Data.University_Grant_Application_SystemContext context)
+        {
+            _context = context;
+        }
+
+        public School School { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var school = await _context.Schools.FirstOrDefaultAsync(m => m.SchoolId == id);
+
+            if (school is not null)
+            {
+                School = school;
+
+                return Page();
+            }
+
+            return NotFound();
+        }
+    }
+}
